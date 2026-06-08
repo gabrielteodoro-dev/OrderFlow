b6915d5137f29d8cf98210b1ffb046fe4f83f440
Gabriel <teodorogabriel1802@gmail.com>
2026-06-08 19:26:35 -0300

    feat: implement UserService and add user findById endpoint

--- src/main/java/com/gabrielteodoro/orderflow/controllers/UserController.java
+++ src/main/java/com/gabrielteodoro/orderflow/controllers/UserController.java
@@ -1,18 +1,29 @@
 package com.gabrielteodoro.orderflow.controllers;
 
 import com.gabrielteodoro.orderflow.entities.User;
+import com.gabrielteodoro.orderflow.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserController {
 
+    @Autowired
+    private UserService service;
+
     @GetMapping
-    public ResponseEntity<User> findAll(){
-        User u = new User(1L,"Maria","maria@gmail.com","99999999","123456");
-        return ResponseEntity.ok().body(u);
+    public ResponseEntity<List<User>> findAll(){
+        List<User> users = service.findAll();
+        return ResponseEntity.ok().body(users);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id){
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
     }
 }
